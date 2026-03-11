dd4be995b1573351c0e1f6bb3ad18ebe99406c13
Gabriel <gaby@gmail.com>
2026-03-11 12:31:12 -0300

    [GO] #86b8qz8hg - fix format - updated version

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule BtrzWebhooksEmitter.MixProject do
   use Mix.Project
 
   @github_url "https://github.com/Betterez/btrz_ex_webhooks_emitter"
-  @version "1.1.0"
+  @version "1.1.1"
 
   def project do
     [
